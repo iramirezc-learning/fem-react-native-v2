43c62cdd0d8a35bf473f76dc12dcf9b2155f9e18
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-09 19:54:45 -0600

    Basic Components: - add styles to Hello World

--- packages/AwesomeProjectRN/App.js
+++ packages/AwesomeProjectRN/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+const globalStyle = { flex: 1 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'lavender',
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: '600',
+  },
+});
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={[globalStyle, styles.container]}>
+        <Text style={styles.text}>Hello World!</Text>
       </View>
     </SafeAreaView>
   );
